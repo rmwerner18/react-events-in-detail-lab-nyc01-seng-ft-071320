bcc70a522a1c3e9bc5fcca3aafad17737f81fac0
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-14 16:38:39 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+class CoordinatesButton extends Component {
+    clickHandler = (event) => {
+        return this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    }
+
+    render() {
+        return <button onClick={this.clickHandler} />
+    }
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+class DelayedButton extends Component {
+
+    clickHandler = (event) => {
+        event.persist()
+        setTimeout(this.props.onDelayedClick, this.props.delay, event)
+    }
+
+    render() {
+        return <button onClick={this.clickHandler} />
+    }
+}
+
+export default DelayedButton
